affc309efc11b9da70e3fbd373f82d2dd4a4bfd3
Kanra Su <karawakara@gmail.com>
2020-06-26 23:00:41 -0700

    update

--- student/src/test/java/edu/pdx/cs410J/huanhua/StudentTest.java
+++ student/src/test/java/edu/pdx/cs410J/huanhua/StudentTest.java
@@ -6,20 +6,30 @@ import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+
 
 /**
- * Unit tests for the Student class.  In addition to the JUnit annotations,
+ * Unit tests for the Student class. In addition to the JUnit annotations,
  * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
  * matchers for more readable assertion statements.
  */
-public class StudentTest
-{
-
-  @Test
-  public void studentNamedPatIsNamedPat() {
-    String name = "Pat";
-    var pat = new Student(name, new ArrayList<>(), 0.0, "Doesn't matter");
-    assertThat(pat.getName(), equalTo(name));
-  }
-
+public class StudentTest {
+    
+    @Test
+    public void studentNamedPatIsNamedPat() {
+        String name = "Pat";
+        var pat = new Student(name, new ArrayList<>(), 0.0, "Doesn't matter");
+        assertThat(pat.getName(), equalTo(name));
+    }
+    
+    @Test
+    public void toStringContainsStudentName() {
+        String name = "Pat";
+        var pat = new Student(name, new ArrayList<>(), 0.0, "Doesn't matter");
+        assertThat(pat.toString(), containsString(name));
+    }
+    
+    
+    
 }
