9e9ba8018ca957776249fa6e34d90c6537cd3f4c
Kanra Su <karawakara@gmail.com>
2020-07-06 15:08:42 -0700

    update koan

--- koans/src/intermediate/AboutDates.java
+++ koans/src/intermediate/AboutDates.java
@@ -1,6 +1,7 @@
 package intermediate;
 
-import com.sandwich.koan.Koan;
+import static com.sandwich.koan.constant.KoanConstants.__;
+import static com.sandwich.util.Assert.assertEquals;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -8,66 +9,80 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import static com.sandwich.koan.constant.KoanConstants.__;
-import static com.sandwich.util.Assert.assertEquals;
+import com.sandwich.koan.Koan;
 
 
 public class AboutDates {
-
-    private Date date = new Date(100010001000L);
-
-    @Koan
-    public void dateToString() {
-        assertEquals(date.toString(), __);
-    }
-
-    @Koan
-    public void changingDateValue() {
-        int oneHourInMiliseconds = 3600000;
-        date.setTime(date.getTime() + oneHourInMiliseconds);
-        assertEquals(date.toString(), __);
-    }
-
-    @Koan
-    public void usingCalendarToChangeDates() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.add(Calendar.MONTH, 1);
-        assertEquals(cal.getTime().toString(), __);
-    }
-
-    @Koan
-    public void usingRollToChangeDatesDoesntWrapOtherFields() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.roll(Calendar.MONTH, 12);
-        assertEquals(cal.getTime().toString(), __);
-    }
-
-    @Koan
-    public void usingDateFormatToFormatDate() {
-        String formattedDate = DateFormat.getDateInstance().format(date);
-        assertEquals(formattedDate, __);
-    }
-
-    @Koan
-    public void usingDateFormatToFormatDateShort() {
-        String formattedDate = DateFormat.getDateInstance(DateFormat.SHORT).format(date);
-        assertEquals(formattedDate, __);
-    }
-
-    @Koan
-    public void usingDateFormatToFormatDateFull() {
-        String formattedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
-        // There is also DateFormat.MEDIUM and DateFormat.LONG... you get the idea ;-)
-        assertEquals(formattedDate, __);
-    }
-
-    @Koan
-    public void usingDateFormatToParseDates() throws ParseException {
-        DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
-        Date date2 = dateFormat.parse("01-01-2000");
-        assertEquals(date2.toString(), __);
-        // What happened to the time? What do you need to change to keep the time as well?
-    }
+	
+	private Date date = new Date(100010001000L);
+	
+	@Koan
+	public void dateToString() {
+		assertEquals(date.toString(), "Sat Mar 03 04:33:21 PST 1973");
+	}
+	
+	@Koan
+	public void changingDateValue() {
+		int oneHourInMiliseconds = 3600000;
+		
+		date.setTime(date.getTime() + oneHourInMiliseconds);
+		assertEquals(date.toString(), "Sat Mar 03 05:33:21 PST 1973");
+	}
+	
+	@Koan
+	public void usingCalendarToChangeDates() {
+		Calendar cal = Calendar.getInstance();
+		
+		cal.setTime(date);
+		cal.add(Calendar.MONTH, 1);
+		
+		assertEquals(cal.getTime().toString(), "Tue Apr 03 05:33:21 PST 1973");
+	}
+	
+	@Koan
+	public void usingRollToChangeDatesDoesntWrapOtherFields() {
+		Calendar cal = Calendar.getInstance();
+		
+		cal.setTime(date);
+		
+		assertEquals(cal.getTime().toString(), "Sat Mar 03 05:33:21 PST 1973");
+		
+		cal.roll(Calendar.MONTH, 12);
+		
+		assertEquals(cal.getTime().toString(), "Sat Mar 03 05:33:21 PST 1973");		// roll back
+		
+		cal.roll(Calendar.MONTH, 1);
+		
+		assertEquals(cal.getTime().toString(), "Tue Apr 03 05:33:21 PST 1973");		// roll 1 month
+	}
+	
+	@Koan
+	public void usingDateFormatToFormatDate() {
+		String formattedDate = DateFormat.getDateInstance().format(date);
+		
+		assertEquals(formattedDate, "1973/03/03");
+	}
+	
+	@Koan
+	public void usingDateFormatToFormatDateShort() {
+		String formattedDate = DateFormat.getDateInstance(DateFormat.SHORT).format(date);
+		
+		assertEquals(formattedDate, "1973/03/03");
+	}
+	
+	@Koan
+	public void usingDateFormatToFormatDateFull() {
+		String formattedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+		
+		// There is also DateFormat.MEDIUM and DateFormat.LONG... you get the idea ;-)
+		assertEquals(formattedDate, "");
+	}
+	
+	@Koan
+	public void usingDateFormatToParseDates() throws ParseException {
+		DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+		Date date2 = dateFormat.parse("01-01-2000");
+		assertEquals(date2.toString(), __);
+		// What happened to the time? What do you need to change to keep the time as well?
+	}
 }
