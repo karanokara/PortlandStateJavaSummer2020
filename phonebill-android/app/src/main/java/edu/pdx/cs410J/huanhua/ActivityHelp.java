b12c4a9cc57166a1555e9f6f3dce4650db8c5e9e
Kanra Su <karawakara@gmail.com>
2020-08-05 19:56:06 -0700

    read readme file for help page

--- phonebill-android/app/src/main/java/edu/pdx/cs410J/huanhua/ActivityHelp.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410J/huanhua/ActivityHelp.java
@@ -1,11 +1,21 @@
 package edu.pdx.cs410J.huanhua;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ActivityHelp extends AppCompatActivity {
+
+    private View thisView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +24,26 @@ public class ActivityHelp extends AppCompatActivity {
 
         TextView view = findViewById(R.id.activity_help_view);
 
-        view.setText("This is a README.");
+        // read file
+        BufferedInputStream bis = new BufferedInputStream(getResources().openRawResource(R.raw.readme)); // buffer the input read from another InputStream
+        // DataInputStream dis = new DataInputStream(bis); // used to read Java's primitive types
+        InputStreamReader isr = new InputStreamReader(bis);
+        BufferedReader br = new BufferedReader(isr);
 
+        String content = "";
+        try {
+            String line = br.readLine();
+            while (line != null) {
+                content += line + "\n";
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            // using any view inside this activity
+            Snackbar.make(view, e.getMessage(), 5000)
+                    .setAction("Action", null).show();
+        }
+
+        view.setText(content);
         // go back to main activity
         // finishActivity(11);
 
